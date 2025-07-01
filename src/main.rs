b1bef9db28c8765ce918283b4216cc17578b6916
hassan <test@gmail.com>
2025-07-01 13:08:41 +0100

    Initial Rust audio API with Axum and Symphonia

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use axum::{routing::get, response::Json, Router};
+use std::{fs::File, net::SocketAddr};
+use symphonia::default::*;
+use symphonia::core::io::MediaSourceStream;
+use serde_json::json;
+
+async fn analyze() -> Json<serde_json::Value> {
+    let file = match File::open("example.mp3") {
+        Ok(f) => f,
+        Err(_) => return Json(json!({ "error": "Fichier non trouvé" })),
+    };
+
+    let mss = MediaSourceStream::new(Box::new(file), Default::default());
+
+    let probed = match get_probe().format(
+        &Default::default(),
+        mss,
+        &Default::default(),
+        &Default::default(),
+    ) {
+        Ok(p) => p,
+        Err(_) => return Json(json!({ "error": "Format non reconnu" })),
+    };
+
+    let format = probed.format;
+
+    Json(json!({
+        "format": format.format_name(),
+        "nb_tracks": format.tracks().len()
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/analyze", get(analyze));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+
+    println!("Serveur lancé sur http://0.0.0.0:8080");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
